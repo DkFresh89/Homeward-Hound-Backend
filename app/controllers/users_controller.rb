bf210570b04ed04695bf8173c374b3c30ce7cc94
Doug <doug.kerzner@gmail.com>
2021-04-28 18:36:29 -0400

    User Login and Sign up built out in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,15 @@ class UsersController < ApplicationController
     end 
 
     def login
-        user = User.find_by(username: params[:username], password_digest: params[:password])
+        user = User.find_by(name: params[:name], password_digest: params[:password_digest])
+        render json: {user: user}
+    end 
+
+    def signup
+        # user_params = params.permit(:username, :password_digest, :rank, :high_score)
+        # new_user = {username: user_params["username"], password_digest: user_params["password_digest"], rank: User.all.length+1, high_score: 0}
+        # user = User.create(new_user)
+        byebug 
     end 
 
 end
