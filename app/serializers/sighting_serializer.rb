412a6a80a3f9db4041a21137d13366bfddc56c8a
Doug <doug.kerzner@gmail.com>
2021-05-04 14:38:54 -0400

    Keeping up with frontend saves

--- app/serializers/sighting_serializer.rb
+++ app/serializers/sighting_serializer.rb
@@ -1,4 +1,6 @@
 class SightingSerializer
   include JSONAPI::Serializer
-  attributes :latitude, :longitude, :description, :time_stamp
+  attributes :latitude, :longitude, :description, :time_stamp, :user, :missing_flyer
+
+  # belongs_to :user
 end
